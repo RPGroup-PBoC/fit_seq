75f87bcd2cbe265fc26e8c7cd0b178d2d120d82e
mrazomej <manuel.razo.m@gmail.com>
2020-07-20 10:31:09 -0700

    include image processing module

--- fit_seq/__init__.py
+++ fit_seq/__init__.py
@@ -5,8 +5,9 @@
 from . import viz
 from . import seq
 from . import popgen
+from . import image
 
-__author__ = """Manuel Razo & Emmanuel Flores"""
+__author__ = """Manuel Razo"""
 __email__ = """mrazomej {at} caltech.edu"""
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 name = 'fit_seq'
